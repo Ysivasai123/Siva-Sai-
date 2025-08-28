6435c73adb1c0f6fe8913ff124be6c38f180d400
Ysivasai123 <sivasaii3512@gmail.com>
2025-08-28 21:46:29 +0530

    My portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+// (Optional) Smooth scroll for navigation
+document.querySelectorAll('.navbar a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if(target) target.scrollIntoView({behavior: 'smooth'});
+  });
+});
